f7b2a8887311609f7a48ce36fe7936ff14855e9d
ZRacerx-Zuhri <zsuryaputra89@gmail.com>
2023-02-06 01:28:06 +0700

    Connect Port

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-require('dotenv').config();
 var express = require('express');
+var app = express();
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const cors = require("cors");
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-const {
-    PORT_API
-} = process.env;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -15,22 +14,41 @@ var balancedinqueryRouter = require('./routes/Inquiry');
 var PPOBRouter = require('./routes/PPOB');
 var tariktunaiRouter = require('./routes/tariktunai');
 
-var app = express();
+const { sequelize } = require("./connection");
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+sequelize
+  .authenticate()
+  .then((db) => {
+    console.log("CONNECTION ESTABLISHED! ");
+  })
+  .catch((err) => {
+    console.error("UNABLE TO ESTABLISH CONNECTION: ", err);
+  });
+
+  const port = process.env.PORT || 5001;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.raw());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/users', usersRouter);
 // app.use('/inqueryacct', inqueryacctRouter);
-app.use('/Inquiry', balancedinqueryRouter);
+app.use('/inquiry', balancedinqueryRouter);
 app.use('/ppob', PPOBRouter);
 app.use('/tariktunai', tariktunaiRouter);
 
+app.get("/", (req, res) => {
+  res.send("gateway-api");
+});
+
 // console.log(`API Run In  at http://localhost:${PORT_API}`);
+app.listen(port, () => {
+    console.log(`Gateway-API listening at http://localhost:${port}`);
+  });
 
 
 module.exports = app;
--- connection/index.js
+++ connection/index.js
@@ -1,24 +1,15 @@
-require('dotenv').config();
+const { DateTime } = require("../utility/dateServer");
 const { Sequelize } = require("sequelize");
-const {
-    DB_USERNAME,
-    DB_PASSWORD,
-    DB_PORT,
-    DB_HOST,
-    DB_DATABASE,
-    DB_DIALECT
-  } = process.env;
 
-
-let name = DB_DATABASE;
-let username = DB_USERNAME;
-let pass = DB_PASSWORD;
-let host = DB_HOST;
+let name = process.env.DBNAME;
+let username = process.env.DBUSERNAME;
+let pass = process.env.DBPASS;
+let host = process.env.DBHOST;
 
 const sequelize = new Sequelize(name, username, pass, {
   host,
-  dialect: DB_DIALECT,
-//   logging: (...msg) => console.log(DateTime() + " ----- " + msg),
+  dialect: "postgres",
+  logging: (...msg) => console.log(DateTime() + " ----- " + msg),
   pool: {
     max: 10000,
     min: 0,
