f699f63ae27d5880af55f16d2ce3257778fe45e1
snooters <cybershane11@gmail.com>
2023-03-04 19:08:25 +0700

    c

--- routes/transfer.js
+++ routes/transfer.js
@@ -541,7 +541,7 @@ router.post('/', async (req, res) => {
                         }
                     }
                 }
-                if (req.body.bpr_id !== "600998") {
+                if (req.body.bank_tujuan !== "600998") {
                     return res.status(200).send({
                         code: "000",
                         status: "SUKSES",
